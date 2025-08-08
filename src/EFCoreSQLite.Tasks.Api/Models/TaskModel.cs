96282601d3a36626da2ca26111e71aebae587cd3
cryan-code1 <cryan.code@gmail.com>
2025-08-08 15:11:16 -0300

    feat: criação do do TaskModel que será utilizado para criação da tabela Task

---
+++ src/EFCoreSQLite.Tasks.Api/Models/TaskModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EFCoreSQLite.Tasks.Api.Models;
+
+public class TaskModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public bool Done { get; set; } = false;
+    public DateTime CreationDate { get; set; }
+    public DateTime ConclusionDate { get; set; }
+}
