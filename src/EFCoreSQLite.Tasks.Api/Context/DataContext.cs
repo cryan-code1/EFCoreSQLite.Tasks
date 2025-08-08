40d37bf1e0c637b2366994a46c99428691ecde4c
cryan-code1 <cryan.code@gmail.com>
2025-08-08 15:10:34 -0300

    feat: criação da classe DataContext para utilização das abstrações EF

---
+++ src/EFCoreSQLite.Tasks.Api/Context/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using EFCoreSQLite.Tasks.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreSQLite.Tasks.Api.Context;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public DbSet<TaskModel> Tasks { get; set; } = null!;
+}
