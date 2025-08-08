2282a6e8fdb5784279e3f0f1e885260f518f25be
cryan-code1 <cryan.code@gmail.com>
2025-08-08 17:49:29 -0300

    feat: alteração da propriedade Done para Status

--- src/EFCoreSQLite.Tasks.Api/Models/TaskModel.cs
+++ src/EFCoreSQLite.Tasks.Api/Models/TaskModel.cs
@@ -1,11 +1,13 @@
-﻿namespace EFCoreSQLite.Tasks.Api.Models;
+﻿using EFCoreSQLite.Tasks.Api.Models.Enums;
+
+namespace EFCoreSQLite.Tasks.Api.Models;
 
 public class TaskModel
 {
     public int Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; } = string.Empty;
-    public bool Done { get; set; } = false;
+    public TaskModelStatus Status { get; set; } = TaskModelStatus.Created;
     public DateTime CreationDate { get; set; }
     public DateTime? ConclusionDate { get; set; }
 }
